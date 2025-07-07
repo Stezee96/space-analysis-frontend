49bf49b70e68359e89779e0e5bdea5c326cc5728
Stezee96 <gosteve308@gmail.com>
2025-07-07 01:48:37 -0400

    reduces CLS annd Remove extra whitespace

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -29,6 +29,7 @@ function Dashboard() {
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <main className="grow">
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
             {/* Title */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
               <div className="mb-4 sm:mb-0">
